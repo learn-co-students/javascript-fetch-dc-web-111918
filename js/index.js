c93d666eb63cfcc5126d16d28006653f34dc243a
rykyou <rkim1214@gmail.com>
2019-01-12 23:08:00 -0500

    Done.

--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
-const token = "8945d69d3c4082a0ee75a12868f698bb329899b4";
+const token = ;
 
-  fetch('https://api.github.com/user/repos', {
-    headers: {
-      Authorization: `token ${token}`
-    }
-  })
-    .then(res => res.json())
-    .then(json => console.log(json));
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+})
+  .then(res => res.json())
+  .then(json => console.log(json));
